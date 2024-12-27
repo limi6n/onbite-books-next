3b146717d355adf9281ef1125754fb81d1931230
anhyelim <6nlimitless@gmail.com>
2024-12-27 18:08:35 +0900

    Data Fetching, Page Cache, Streaming

--- src/app/(with-searchbar)/layout.tsx
+++ src/app/(with-searchbar)/layout.tsx
@@ -4,7 +4,9 @@ import Searchbar from "../../components/searchbar";
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div>
-      <Searchbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Searchbar />
+      </Suspense>
       {children}
     </div>
   );
--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,21 +1,62 @@
 import BookItem from "@/components/book-item";
 import style from "./page.module.css";
-import books from "@/mock/books.json";
+import { BookData } from "@/types";
+import { Suspense } from "react";
+import BookListSkeleton from "@/components/skeleton/book-list-skeleton";
+
+async function AllBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+    { cache: "force-cache" }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allBooks: BookData[] = await response.json();
+
+  return (
+    <div>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecBooks() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const recBooks: BookData[] = await response.json();
+  return (
+    <div>
+      {recBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <Suspense fallback={<BookListSkeleton count={3} />}>
+          <RecBooks />
+        </Suspense>
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
+        <Suspense fallback={<BookListSkeleton count={10} />}>
+          <AllBooks />
+        </Suspense>
       </section>
     </div>
   );
